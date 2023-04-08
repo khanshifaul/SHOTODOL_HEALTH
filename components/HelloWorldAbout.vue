ae252a350b957a4a82affb024ac6e728f61a21e9
Shifaul Islam <37363422+khanshifaul@users.noreply.github.com>
2023-04-08 10:23:15 +0600

    new file:   .github/workflows/main.yml 	modified:   components/HelloWorldAbout.vue 	new file:   deploy.sh

--- components/HelloWorldAbout.vue
+++ components/HelloWorldAbout.vue
@@ -2,17 +2,23 @@
     <section id="about" class="container mx-auto py-4">
         <h3 class="border-b font-Broadway-bt text-3xl pb-2">About us</h3>
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo labore, error minima autem ullam nesciunt
-                sit
-                dolor ratione inventore provident necessitatibus corporis voluptas quasi rem. Possimus quod aut totam
-                necessitatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo labore, error minima autem ullam nesciunt
-                sit
-                dolor ratione inventore provident necessitatibus corporis voluptas quasi rem. Possimus quod aut totam
-                necessitatibus?</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo labore, error minima autem ullam nesciunt
-                sit
-                dolor ratione inventore provident necessitatibus corporis voluptas quasi rem. Possimus quod aut totam
-                necessitatibus?</p>
+            <p>Welcome to [Hospital Name], where compassionate care and state-of-the-art medical technology meet to provide
+                the best possible healthcare services to our patients.</p>
+            <p> We have a team of highly skilled and experienced medical
+                professionals, including doctors, nurses, and support staff, who are committed to delivering exceptional
+                care to every patient.</p>
+            <p> At [Hospital Name], we believe that every patient deserves the best possible treatment, and we strive to
+                provide personalized care to meet their unique healthcare needs. We understand that healthcare can be a
+                stressful and overwhelming experience, which is why we prioritize communication, transparency, and empathy
+                in all of our interactions with our patients.</p>
+            <p> Our hospital is equipped with state-of-the-art medical technology and facilities, including advanced
+                diagnostic equipment and modern treatment options. We offer a wide range of services, including emergency
+                care, surgery, obstetrics and gynecology, cardiology, neurology, pediatrics, oncology, and more.</p>
+            <p> We are dedicated to the continuous improvement of our services and facilities, and we regularly invest in
+                the latest medical technologies and training for our staff. We are committed to delivering the highest
+                standards of healthcare, and we strive to exceed the expectations of our patients and their families.</p>
+            <p> Thank you for considering [Hospital Name] for your healthcare needs. We look forward to serving you and
+                providing you with the compassionate care and exceptional medical services that you deserve.</p>
         </div>
-</section></template>
+    </section>
+</template>
