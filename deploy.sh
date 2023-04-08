973c6b7b16d4b1746765afa84bad6617d23bd1f1
Shifaul Islam <37363422+khanshifaul@users.noreply.github.com>
2023-04-08 11:21:40 +0600

    commit

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:khanshifaul/SHOTODOL_HEALTH.git main:gh-pages
+git push -f git@github.com:khanshifaul/SHOTODOL_HEALTH.git main:gh-pages
 
 cd -
